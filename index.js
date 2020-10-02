8c3f5667b9603762f24a73f2f9b94cac4923aafa
Aminu Yiwere <43513230+yiwereaminu@users.noreply.github.com>
2020-10-02 03:50:19 +0000

    edits

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// function changeImage(){
-//   var newImage=document.querySelector(".img1").outerHTML;
-//   newImage.outerHTML = "<img class='img1' src='images/dice5.png'>"
-// }
 
 
 var imageArray = new Array();
@@ -25,25 +21,7 @@ imageArray[5] = new Image();
 imageArray[5].src = "images/dice6.png";
 
 
-/*------------------------------------*/
 
-// function nextImage(element)
-// {
-//     var img = document.getElementById(element);
-
-//     for(var i = 0; i < imgArray.length;i++)
-//     {
-//         if(imgArray[i].src == img.src) // << check this
-//         {
-//             if(i === imgArray.length){
-//                 document.getElementById(element).src = imgArray[0].src;
-//                 break;
-//             }
-//             document.getElementById(element).src = imgArray[i+1].src;
-//             break;
-//         }
-//     }
-// }
 
 // playerOne
 var randomNumber1 = Math.floor(Math.random()*6) +1;
